4696dbc2bb80e600b73a1009211b7e3fb191c2a3
pawa <>
2018-10-07 13:43:35 +0300

    Update CSharp/AppOne/AppOne/Program.cs

--- CSharp/AppOne/AppOne/Program.cs
+++ CSharp/AppOne/AppOne/Program.cs
@@ -6,9 +6,44 @@ namespace AppOne
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите своё имя: ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Привет {name}");
+            double a;
+            double b;
+            double result;
+            char operation;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите действие");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (operation == '+')
+            {
+                result = a + b;
+                Console.WriteLine($"Сумма {a} и {b} равна {result}");
+            }
+            else if (operation == '-')
+            {
+                result = a - b;
+                Console.WriteLine($"Вычитание {a} и {b} равно {result}");
+            }
+            else if (operation == '/')
+            {
+                result = a / b;
+                Console.WriteLine($"Деление {a} на {b} равно {result}");
+            }
+            else if (operation == '*')
+            {
+                result = a * b;
+                Console.WriteLine($"Умножение {a} на {b} равно {result}");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор");
+            }
             Console.ReadKey();
         }
     }
