72816431480f84381a299ddf557edd408cfdf495
pawa <>
2018-10-07 12:30:21 +0300

    Update CSharp/AppOne/AppOne/Program.cs

---
+++ CSharp/AppOne/AppOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+            Console.ReadKey();
+        }
+    }
+}
