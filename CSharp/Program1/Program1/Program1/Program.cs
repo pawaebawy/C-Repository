1a9b9b3c19f9611ddc66064f7f3941f006bee6e4
User of DK12 <DK12@it.isuct.ru>
2018-10-09 12:59:22 +0400

    Program1

---
+++ CSharp/Program1/Program1/Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            double a = 1.1;
+            double b = 0.09;
+            for (double i = 1.2; i <= 2.2; i = i + 0.2)
+            {
+                y = (Math.Log((Math.Pow(i,2))-1)) / (Math.Log10(a*Math.Pow(i,2)-b) / Math.Log10(5));
+                Console.WriteLine($"Для шага {i} функция y = {Math.Round(y, 4)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
